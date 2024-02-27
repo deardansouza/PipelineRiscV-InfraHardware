b23654215c4db32ea21942fd010a190644a78372
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-02-26 23:50:42 -0300

    Update ALUController.sv

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -11,18 +11,29 @@ module ALUController (
 );
 
   assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||  // R\I-xor
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I-srli
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) & (Funct7 == 7'b0100000))  || //R\I - sub
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I-srai
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)); // Branch - bne
 
   assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add/addi
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)) || //R\I - sub
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) || // R\I-srai
+      (ALUOp == 2'b11) ||  //LUI
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101));    // Branch - bge
 
-  assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+  assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I-srli
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I-srai
+      ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-slli
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-slt/slti
+      ((ALUOp == 2'b01) && (Funct3 == 3'b001)) ||  // Branch - bne
+      ((ALUOp == 2'b01) && (Funct3 == 3'b100)) ||  // Branch - blt
+      ((ALUOp == 2'b01) && (Funct3 == 3'b101));    // Branch - bge
 
-  assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+  assign Operation[3] = (ALUOp == 2'b01) ||  // Branch - beq/bne/blt/bge
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)) ||  // R\I-xor
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010)) ||  // R\I-slt/slti
+      (ALUOp == 2'b11);  //LUI
 endmodule

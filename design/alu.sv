e4275a9556ceae2557fb50dd51dfc958a177387c
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-02-26 23:42:34 -0300

    Update alu.sv

--- design/alu.sv
+++ design/alu.sv
@@ -17,10 +17,30 @@ module alu#(
             case(Operation)
             4'b0000:        // AND
                     ALUResult = SrcA & SrcB;
+	    4'b0001:	    // OR
+		    ALUResult = SrcA | SrcB;
             4'b0010:        // ADD
                     ALUResult = SrcA + SrcB;
+	    4'b0011:	    // SUB
+		    ALUResult = SrcA - SrcB;
+	    4'b0100:	    // SLLI
+		    ALUResult = SrcA << SrcB[4:0];
+	    4'b0101:	    // SRLI
+		    ALUResult = SrcA >> SrcB[4:0];
+	    4'b0111:	    // SRAI
+		    ALUResult = $signed(SrcA) >>> SrcB[4:0];
             4'b1000:        // Equal
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+	    4'b1001:	    // XOR
+		    ALUResult = SrcA ^ SrcB;
+	    4'b1010:	    // LUI
+		    ALUResult = 32'b1;
+	    4'b1100:	    // LessThan
+		    ALUResult = (SrcA < SrcB) ? 1 : 0;
+	    4'b1101:	    // DifferentThan
+		    ALUResult = (SrcA != SrcB) ? 1 : 0;
+	    4'b1110:	    // GreaterOrEqualThan
+		    ALUResult = (SrcA >= SrcB) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase

9517c57eea7eaa61a01f940f73941f279b427983
mwsa2 <114543550+mwsa2@users.noreply.github.com>
2024-03-01 16:56:31 -0300

    Update Controller.sv

--- design/Controller.sv
+++ design/Controller.sv
@@ -16,31 +16,33 @@ module Controller (
     output logic MemRead,  //Data memory contents designated by the address input are put on the Read data output
     output logic MemWrite, //Data memory contents designated by the address input are replaced by the value on the Write data input.
     output logic [1:0] ALUOp,  //00: LW/SW; 01:Branch; 10: Rtype
-    output logic Branch  //0: branch is not taken; 1: branch is taken
+    output logic Branch,  //0: branch is not taken; 1: branch is taken
+    output logic [1:0] RWSel,
+    output logic jalrsel
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR, JAL, JALR. I_TYPE, HALT, U_TYPE;
+  logic [6:0] R_TYPE, I_TYPE , U_TYPE, LW, SW, BR, JAL, JALR, HALT;
 
-  assign R_TYPE = 7'b0110011;  //add,and, or, xor, sub
-  assign LW = 7'b0000011;  //lw, lb, lh, lbu 
-  assign SW = 7'b0100011;  //sw, sh, sb
-  assign BR = 7'b1100011;  //beq, bne, blt, bge
-  assign JAL = 7'b1101111;  //jal
-  assign JALR = 7'b1100111;  //jalr 
-  assign I_TYPE = 7'b0010011; //addi, slli, srai, srli, slti
-  assign HALT = 7'b0000001; //halt  
+  assign R_TYPE = 7'b0110011;  //add,sub,and,or,xor
+  assign I_TYPE = 7'b0010011; //slti,slli,srli,srai,addi
   assign U_TYPE = 7'b0110111; //lui
+  assign LW = 7'b0000011;  //lw,lb,lh,lbu
+  assign SW = 7'b0100011;  //sw,sb,sh
+  assign BR = 7'b1100011;  //beq,bne,bge,blt
+  assign JAL = 7'b1101111; //jal
+  assign JALR = 7'b1100111; //jalr
+  assign HALT = 7'b0000001; //halt
 
 
   assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == JAL || Opcode == JALR );
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR);
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
   assign ALUOp[0] = (Opcode == BR || Opcode == U_TYPE || Opcode == JAL || Opcode == HALT);
-  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == LW || Opcode == JAL || Opcode == JALR || Opcode == U_TYPE);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == I_TYPE || Opcode == U_TYPE || Opcode == JAL || Opcode == JALR || Opcode == HALT);
   assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALR || Opcode == HALT);
-  assign RWSel[0] = (Opcode == JAL || Opcode == JALR || Opcode ++ U_TYPE);
-  assign RWSel[0] = (Opcode == U_TYPE);
-  assign JalrSel =  (Opcode == JALR);
+  assign RWSel[0] = (Opcode == JAL || Opcode == JALR || Opcode == U_TYPE);
+  assign RWSel[1] = (Opcode == U_TYPE);
+  assign jalrsel = (Opcode == JALR);
 endmodule

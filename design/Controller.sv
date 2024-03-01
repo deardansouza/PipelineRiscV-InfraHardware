261dbf2c465ed837e380ae3ea85ab3769c66372c
Danilo Souza <57498224+deardansouza@users.noreply.github.com>
2024-03-01 11:53:32 -0300

    Update Controller.sv

--- design/Controller.sv
+++ design/Controller.sv
@@ -19,19 +19,28 @@ module Controller (
     output logic Branch  //0: branch is not taken; 1: branch is taken
 );
 
-  logic [6:0] R_TYPE, LW, SW, BR;
+  logic [6:0] R_TYPE, LW, SW, BR, JAL, JALR. I_TYPE, HALT, U_TYPE;
 
-  assign R_TYPE = 7'b0110011;  //add,and
-  assign LW = 7'b0000011;  //lw
-  assign SW = 7'b0100011;  //sw
-  assign BR = 7'b1100011;  //beq
+  assign R_TYPE = 7'b0110011;  //add,and, or, xor, sub
+  assign LW = 7'b0000011;  //lw, lb, lh, lbu 
+  assign SW = 7'b0100011;  //sw, sh, sb
+  assign BR = 7'b1100011;  //beq, bne, blt, bge
+  assign JAL = 7'b1101111;  //jal
+  assign JALR = 7'b1100111;  //jalr 
+  assign I_TYPE = 7'b0010011; //addi, slli, srai, srli, slti
+  assign HALT = 7'b0000001; //halt  
+  assign U_TYPE = 7'b0110111; //lui
 
-  assign ALUSrc = (Opcode == LW || Opcode == SW);
+
+  assign ALUSrc = (Opcode == LW || Opcode == SW || Opcode == I_TYPE || Opcode == JALR);
   assign MemtoReg = (Opcode == LW);
-  assign RegWrite = (Opcode == R_TYPE || Opcode == LW);
+  assign RegWrite = (Opcode == R_TYPE || Opcode == LW || Opcode == I_TYPE || Opcode == JAL || Opcode == JALR );
   assign MemRead = (Opcode == LW);
   assign MemWrite = (Opcode == SW);
-  assign ALUOp[0] = (Opcode == BR);
-  assign ALUOp[1] = (Opcode == R_TYPE);
-  assign Branch = (Opcode == BR);
+  assign ALUOp[0] = (Opcode == BR || Opcode == U_TYPE || Opcode == JAL || Opcode == HALT);
+  assign ALUOp[1] = (Opcode == R_TYPE || Opcode == LW || Opcode == JAL || Opcode == JALR || Opcode == U_TYPE);
+  assign Branch = (Opcode == BR || Opcode == JAL || Opcode == JALR || Opcode == HALT);
+  assign RWSel[0] = (Opcode == JAL || Opcode == JALR || Opcode ++ U_TYPE);
+  assign RWSel[0] = (Opcode == U_TYPE);
+  assign JalrSel =  (Opcode == JALR);
 endmodule
